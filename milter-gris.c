bb6401c83812bd38df3befde32e10efe7538be8d
Anthony Howe <achowe@snert.com>
2012-01-03 13:35:57 +0100

    Configure and build changes to reflect changes in libsnert.

--- milter-gris.c
+++ milter-gris.c
@@ -7,7 +7,7 @@
  *	darkness and the light."
  *	 - Delenn, All Alone in the Night, Babylon 5
  *
- * Copyright 2004, 2006 by Anthony Howe. All rights reserved.
+ * Copyright 2004, 2012 by Anthony Howe. All rights reserved.
  *
  * The following should be added to the sendmail.mc file:
  *
@@ -128,8 +128,8 @@
 #include <com/snert/lib/util/setBitWord.h>
 #include <com/snert/lib/sys/Time.h>
 
-#if LIBSNERT_MAJOR < 1 || LIBSNERT_MINOR < 63
-# error "LibSnert/1.63 or better is required"
+#if LIBSNERT_MAJOR < 1 || LIBSNERT_MINOR < 75
+# error "LibSnert 1.75.8 or better is required"
 #endif
 
 #ifdef MILTER_BUILD_STRING
